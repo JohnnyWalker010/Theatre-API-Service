48268af0a8424f084cbf6416ada7abf997b7194e
Yevhen <evgeniy.playid@icloud.com>
2024-03-27 11:10:42 +0200

    Create models.py for theatre_booking app

--- theatre_service/theatre_booking/models.py
+++ theatre_service/theatre_booking/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.SET_NULL)
+    theatre_hall = models.ManyToManyField(TheatreHall, on_delete=models.SET_NULL)
+    showtime = models.DateTimeField()
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.SET_NULL)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
