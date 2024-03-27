00efecf42b4ea645b9c8564639e2e184aaf531b9
Yevhen <evgeniy.playid@icloud.com>
2024-03-27 18:02:28 +0200

    Add possibility to create ticket while creating reservation

--- theatre_booking/admin.py
+++ theatre_booking/admin.py
@@ -10,9 +10,19 @@ from theatre_booking.models import (
     Ticket
 )
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(Actor)
 admin.site.register(Genre)
-admin.site.register(Reservation)
 admin.site.register(TheatreHall)
 admin.site.register(Play)
 admin.site.register(Performance)
