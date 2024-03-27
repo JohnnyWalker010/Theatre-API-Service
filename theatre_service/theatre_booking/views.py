60e2f3e93170c7cd3279b0d283520a7e6203c3a6
Yevhen <evgeniy.playid@icloud.com>
2024-03-27 14:13:59 +0200

    Add views

--- theatre_service/theatre_booking/views.py
+++ theatre_service/theatre_booking/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre_service.theatre_booking.models import Actor, Genre, Reservation, TheatreHall, Play, Performance, Ticket
+from theatre_service.theatre_booking.serializers import ActorSerializer, GenreSerializer, ReservationSerializer, \
+    TheatreHallSerializer, PlaySerializer, PerformanceSerializer, TicketSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
