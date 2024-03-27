77857ccea5aa239dad99c7627e3a5e701e9dde27
Yevhen <evgeniy.playid@icloud.com>
2024-03-27 14:27:34 +0200

    Add urls.py and register it in project urls

---
+++ theatre_service/theatre_booking/urls.py
@@ -0,0 +1,25 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from theatre_service.theatre_booking.views import (
+    ActorViewSet,
+    GenreViewSet,
+    ReservationViewSet,
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+    TicketViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("ticket", TicketViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
--- theatre_service/theatre_service/urls.py
+++ theatre_service/theatre_service/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/theatre-booking", include("theatre_booking.urls", namespace="theatre_booking")),
 ]
