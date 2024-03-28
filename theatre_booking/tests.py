ffb3e9c94d8255fa36b89864bf9ccebfdd4996ea
Yevhen <evgeniy.playid@icloud.com>
2024-03-28 14:19:57 +0200

    Add models' tests

--- theatre_booking/tests.py
+++ theatre_booking/tests.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -101,3 +104,73 @@ class AdminPanelTestCase(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertFalse(Actor.objects.filter(id=actor_id).exists())
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword1"
+        )
+        self.actor = Actor.objects.create(first_name="John", last_name="Doe")
+        self.genre = Genre.objects.create(name="Drama")
+        self.hall = TheatreHall.objects.create(
+            name="Test Hall", rows=15, seats_in_row=15
+        )
+        self.play = Play.objects.create(
+            title="Test play", description="Test description"
+        )
+        self.play.actors.add(self.actor)
+        self.play.genres.add(self.genre)
+        self.performance = Performance.objects.create(
+            play=self.play, theatre_hall=self.hall, showtime=timezone.now()
+        )
+        self.reservation = Reservation.objects.create(user=self.user)
+        self.ticket = Ticket.objects.create(
+            row=5, seat=10, performance=self.performance, reservation=self.reservation
+        )
+
+    def test_actor_str(self):
+        actor = Actor.objects.get(first_name="John")
+        self.assertEqual(str(actor), "John Doe")
+
+    def test_genre_str(self):
+        genre = Genre.objects.get(name="Drama")
+        self.assertEqual(str(genre), "Drama")
+
+    def test_reservation_str(self):
+        reservation = Reservation.objects.get(user=self.user)
+        self.assertEqual(
+            str(reservation), f"Reservation of {self.user} at {reservation.created_at}"
+        )
+
+    def test_theatre_hall_str(self):
+        hall = TheatreHall.objects.get(name="Test Hall")
+        self.assertEqual(str(hall), "Test Hall with 225 capacity")
+
+    def test_play_str(self):
+        play = Play.objects.get(title="Test play")
+        self.assertEqual(str(play), "Test play")
+
+    def test_performance_str(self):
+        performance = Performance.objects.get(play=self.play)
+        self.assertEqual(str(performance), f"Performance at {performance.showtime}")
+
+    def test_ticket_str(self):
+        ticket = Ticket.objects.get(id=self.ticket.id)
+        expected_str = (
+            f"Ticket {ticket.id}, Performance: Test play, Customer: testuser, Seat: 10, Row: 5, Purchased "
+            f"at: {ticket.reservation.created_at}."
+        )
+        self.assertEqual(str(ticket), expected_str)
+
+    def test_ticket_clean(self):
+        invalid_ticket = Ticket(
+            row=20, seat=30, performance=self.performance, reservation=self.reservation
+        )
+        self.assertRaises(ValidationError, invalid_ticket.clean)
+
+    def test_ticket_clean_no_performance(self):
+        ticket_without_performance = Ticket(
+            row=5, seat=10, reservation=self.reservation
+        )
+        self.assertRaises(ValidationError, ticket_without_performance.clean)
